b40357fe064417f0e08d9cf419231de6bcbe9d55
Or Perry <or@orperry.com>
2023-07-15 16:11:48 +0300

    navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('../modules/home-page/home-page.module').then(
+        (m) => m.HomePageModule,
+      ),
+  },
+  {
+    path: 'אודותי',
+    loadChildren: () =>
+      import('../modules/about-me/about-me.module').then(
+        (m) => m.AboutMeModule,
+      ),
+  },
+  {
+    path: 'מאמרים',
+    loadChildren: () =>
+      import('../modules/publications/publications.module').then(
+        (m) => m.PublicationsModule,
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
